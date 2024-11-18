7f3d75134dace023053b461706b99317a9750a68
Sriram <sgcuber24@gmail.com>
2024-11-18 09:11:35 +0530

    Login converted to react hook form

--- src/routes/_public-layout/login.lazy.tsx
+++ src/routes/_public-layout/login.lazy.tsx
@@ -1,25 +1,47 @@
-import { Alert } from "@/components/ui/alert";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 import { Button } from "@/components/ui/button";
 import ErrorMessage from "@/components/ui/error-message";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import supabase from "@/supabaseClient";
 import { createLazyFileRoute, Link } from "@tanstack/react-router";
-import React, { useState } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
 
 export const Route = createLazyFileRoute("/_public-layout/login")({
 	component: Login,
 });
 
+const formSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8, "Password must be at least 8 characters"),
+});
+
 function Login() {
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
 	const [error, setError] = useState<string | null>(null);
 	const [loading, setLoading] = useState<boolean>(false);
 
-	const handleLogin = async (e: React.FormEvent) => {
-		e.preventDefault();
+	const form = useForm<z.infer<typeof formSchema>>({
+		resolver: zodResolver(formSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const handleLogin = async (values: z.infer<typeof formSchema>) => {
+		setError(null);
 		setLoading(true);
+		const { email, password } = values;
 		const { error } = await supabase.auth.signInWithPassword({
 			email,
 			password,
@@ -42,32 +64,62 @@ function Login() {
 					Create one
 				</Link>
 			</p>
-			<form className="mt-8 w-full" onSubmit={handleLogin}>
-				<Label>
-					Email
-					<Input
-						type="email"
-						value={email}
-						onChange={e => setEmail(e.target.value)}
-						required
+			<Form {...form}>
+				<form
+					className="mt-8 w-full"
+					onSubmit={e => {
+						e.preventDefault();
+						setError(null);
+						form.handleSubmit(handleLogin)();
+					}}
+				>
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input placeholder="Email" {...field} />
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
 					/>
-				</Label>
-				<div className="mt-4">
-					<Label>
-						Password
-						<Input
-							type="password"
-							value={password}
-							onChange={e => setPassword(e.target.value)}
-							required
+					<div className="mt-4">
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel className="mb-0">
+										Password
+									</FormLabel>
+									<FormControl>
+										<Input
+											type="password"
+											placeholder="Password"
+											{...field}
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
 						/>
-					</Label>
-				</div>
-				{error && <ErrorMessage title="Login Error" message={error} />}
-				<Button loading={loading} className="mt-6 w-full" type="submit">
-					Login
-				</Button>
-			</form>
+					</div>
+
+					{error && (
+						<ErrorMessage title="Login Error" message={error} />
+					)}
+					<Button
+						loading={loading}
+						className="mt-6 w-full"
+						type="submit"
+					>
+						Login
+					</Button>
+				</form>
+			</Form>
 		</div>
 	);
 }
